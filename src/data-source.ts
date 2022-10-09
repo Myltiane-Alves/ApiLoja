d0b259f4b2df9f6292b102af44b68e9630e1e0b9
Myltiane <myltiane.aux@gmail.com>
2022-10-09 14:31:45 -0300

    feat: toda api estruturada

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,12 +6,13 @@ export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
+    username: "root",
+    password: "95325414",
+    database: "loja",
     synchronize: true,
     logging: false,
-    entities: [User],
-    migrations: [],
+    entities: [""],
+    migrations: ["src/migration/**/*.ts"],
     subscribers: [],
+    
 })

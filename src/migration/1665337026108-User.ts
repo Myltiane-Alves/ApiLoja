ffb922c2285c9ce5de71b9714134ee3b405ea2e6
Myltiane <myltiane.aux@gmail.com>
2022-10-14 12:25:25 -0300

    feat: criando banco de usuários

---
+++ src/migration/1665337026108-User.ts
@@ -0,0 +1,59 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class User1665337026108 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "user",
+            columns: [
+                {
+                    name: "id",
+                    type: "int",
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: "increment",
+                },
+                {
+                    name: "userName",
+                    isUnique: true,
+                    type: "varchar",
+                    length: "255",
+                    isNullable: false,
+                },
+                {
+                    name: "password", 
+                    type: "varchar",
+                    length: "255",
+                    isNullable: false,      
+                },
+                {
+                    name: "name",
+                    type: "varchar",
+                    length: "255",
+                    isNullable: false,
+                },
+                {
+                    name: 'phone',
+                    type: 'varchar',
+                    length: '36',
+                },
+                {
+                    name: 'createdAt',
+                    type: 'datetime',
+                    default: 'CURRENT_TIMESTAMP',
+                },
+                {
+                    name: 'updatedAt',
+                    type: 'datetime',
+                    default: 'CURRENT_TIMESTAMP',
+                }
+
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("user")
+    }
+
+}

8a40619043e3f61277dfc2030f72ec41bd5de5e9
Myltiane <myltiane.aux@gmail.com>
2022-10-14 14:27:07 -0300

    feat: criando tabelas de order detail payment

--- src/typeorm/migration/1665766403288-OrderDetails.ts
+++ src/typeorm/migration/1665766403288-OrderDetails.ts
@@ -1,11 +1,60 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
 
 export class OrderDetails1665766403288 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "orderDetails",
+            columns: [
+                {
+                    name: "id",
+                    type: "int",
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: "increment"
+                },
+                {
+                    name: "userId",
+                    type: "int",
+                    isNullable: false
+                },
+                {
+                    name: "total",
+                    type: "decimal",
+                    precision: 12,
+                    scale: 4,
+                    isNullable: false
+                },
+                {
+                    name: "paymentId",
+                    type: "int",
+                    isNullable: false
+                },
+                {
+                    name: "createdAt",
+                    type: "timestamp",
+                    default: "CURRENT_TIMESTAMP"
+                },
+                {
+                    name: "updatedAt",
+                    type: "timestamp",
+                    default: "CURRENT_TIMESTAMP"
+                },
+            ]
+        }))
+
+        await queryRunner.createForeignKey("orderDetails", new TableForeignKey({
+            columnNames: ["userId"],
+            referencedColumnNames: ["id"],
+            referencedTableName: "user",
+            name: "FK_orderDetails_User",
+            onDelete: "CASCADE"
+        }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey("orderDetails", "FK_orderDetails_User");
+        await queryRunner.dropTable("orderDetails");
     }
 
 }

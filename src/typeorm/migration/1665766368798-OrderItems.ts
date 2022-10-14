a6d46f8c3578a681499261f6d14c51e49a632209
Myltiane <myltiane.aux@gmail.com>
2022-10-14 14:30:31 -0300

    feat: criando tabelas de order items

--- src/typeorm/migration/1665766368798-OrderItems.ts
+++ src/typeorm/migration/1665766368798-OrderItems.ts
@@ -1,11 +1,49 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class OrderItems1665766368798 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "orderItems",
+            columns: [
+                {
+                    name: "id",
+                    type: "int",
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: "increment"
+                },
+                {
+                    name: "orderId",
+                    type: "int",
+                    isNullable: false
+                },
+                {
+                    name: "productId",
+                    type: "int",
+                    isNullable: false
+                },
+                {
+                    name: "quantity",
+                    type: "int",
+                    isNullable: false
+                },
+                {
+                    name: "createdAt",
+                    type: "timestamp",
+                    default: "CURRENT_TIMESTAMP"
+                },
+                {
+                    name: "updatedAt",
+                    type: "timestamp",
+                    default: "CURRENT_TIMESTAMP"
+                }
+            ]
+        }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("orderItems");
     }
 
 }
